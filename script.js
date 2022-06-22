d8a54fe3de6bd9e2bc0a70c43051c0d8a8b5bc55
Rancor38 <zak.rancor@hotmail.com>
2022-06-22 12:10:46 -0700

    added an event listener to box 1 to say click me in the console log

---
+++ script.js
@@ -0,0 +1,9 @@
+//Sanity Check
+// console.log("Hallo")
+
+//CONST ELEMENTS
+const square1 = document.querySelector("#box1")
+
+
+
+square1.addEventListener("click", () => {console.log("You clicked me!")})
