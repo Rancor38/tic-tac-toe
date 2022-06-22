74df363ad6de486f2c87992e433e9c48e9876a45
Rancor38 <zak.rancor@hotmail.com>
2022-06-22 12:13:10 -0700

    added listeners to each box, console log

--- script.js
+++ script.js
@@ -3,7 +3,23 @@
 
 //CONST ELEMENTS
 const square1 = document.querySelector("#box1")
+const square2 = document.querySelector("#box2")
+const square3 = document.querySelector("#box3")
+const square4 = document.querySelector("#box4")
+const square5 = document.querySelector("#box5")
+const square6 = document.querySelector("#box6")
+const square7 = document.querySelector("#box7")
+const square8 = document.querySelector("#box8")
+const square9 = document.querySelector("#box9")
 
 
-
+//EVENT LISTENERS
 square1.addEventListener("click", () => {console.log("You clicked me!")})
+square2.addEventListener("click", () => {console.log("You clicked me!")})
+square3.addEventListener("click", () => {console.log("You clicked me!")})
+square4.addEventListener("click", () => {console.log("You clicked me!")})
+square5.addEventListener("click", () => {console.log("You clicked me!")})
+square6.addEventListener("click", () => {console.log("You clicked me!")})
+square7.addEventListener("click", () => {console.log("You clicked me!")})
+square8.addEventListener("click", () => {console.log("You clicked me!")})
+square9.addEventListener("click", () => {console.log("You clicked me!")})
