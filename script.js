bd21d538dc259bd8407a1a80b0bbac0981dcca04
Rancor38 <zak.rancor@hotmail.com>
2022-06-22 14:57:07 -0700

    got the colors to alternate, by changing the event listener to a single set for both colors, and giving them the property 'y' as a placeholder for whichever square is input as the parameter

--- script.js
+++ script.js
@@ -13,12 +13,16 @@ const square8 = document.querySelector("#box8")
 const square9 = document.querySelector("#box9")
 
 //FUNCTION TO ALTERNATE COLOR CHANGE
+//state is the state of the switch between red and blue
 let state = false
+//y is the input for which square is being clicked/changed in color
+let y
 
-function colorClick() {
+function colorClick(y) {
     if (state == false) {
         //make them red
         console.log("red")
+        y.classList.add('red')
         //and flip the switch
         state = true
         return
@@ -26,6 +30,7 @@ function colorClick() {
     if (state == true) {
         //make them blue
         console.log("blue")
+        y.classList.add('blue')
         //and flip the switch
         state = false
         return
@@ -40,26 +45,13 @@ const redClick = function(x) {
 const blueClick = function(x) {
     x.classList.add('blue')
 }
-
-square1.addEventListener("click", colorClick)
-//EVENT LISTENERS
-// //MAKE THEM RED
-// square1.addEventListener("click", () => {redClick(square1)})
-// square2.addEventListener("click", () => {redClick(square2)})
-// square3.addEventListener("click", () => {redClick(square3)})
-// square4.addEventListener("click", () => {redClick(square4)})
-// square5.addEventListener("click", () => {redClick(square5)})
-// square6.addEventListener("click", () => {redClick(square6)})
-// square7.addEventListener("click", () => {redClick(square7)})
-// square8.addEventListener("click", () => {redClick(square8)})
-// square9.addEventListener("click", () => {redClick(square9)})
-// //MAKE THEM BLUE
-// square1.addEventListener("click", () => {blueClick(square1)})
-// square2.addEventListener("click", () => {blueClick(square2)})
-// square3.addEventListener("click", () => {blueClick(square3)})
-// square4.addEventListener("click", () => {blueClick(square4)})
-// square5.addEventListener("click", () => {blueClick(square5)})
-// square6.addEventListener("click", () => {blueClick(square6)})
-// square7.addEventListener("click", () => {blueClick(square7)})
-// square8.addEventListener("click", () => {blueClick(square8)})
-// square9.addEventListener("click", () => {blueClick(square9)})
+//FUNCTION TO ALTERNATE COLOR TEST LISTENER
+square1.addEventListener("click", () => {colorClick(square1), y})
+square2.addEventListener("click", () => {colorClick(square2)})
+square3.addEventListener("click", () => {colorClick(square3)})
+square4.addEventListener("click", () => {colorClick(square4)})
+square5.addEventListener("click", () => {colorClick(square5)})
+square6.addEventListener("click", () => {colorClick(square6)})
+square7.addEventListener("click", () => {colorClick(square7)})
+square8.addEventListener("click", () => {colorClick(square8)})
+square9.addEventListener("click", () => {colorClick(square9)})
