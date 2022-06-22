b059a9c8d95631de82cdc70ecff69eb036d562f6
Rancor38 <zak.rancor@hotmail.com>
2022-06-22 14:20:27 -0700

    added a switch to alternate and console log red, blue, red, blue, etc

--- script.js
+++ script.js
@@ -13,7 +13,24 @@ const square8 = document.querySelector("#box8")
 const square9 = document.querySelector("#box9")
 
 //FUNCTION TO ALTERNATE COLOR CHANGE
+let state = false
 
+function colorClick() {
+    if (state == false) {
+        //make them red
+        console.log("red")
+        //and flip the switch
+        state = true
+        return
+    }
+    if (state == true) {
+        //make them blue
+        console.log("blue")
+        //and flip the switch
+        state = false
+        return
+    }
+}
 
 //FUNCTION TO CHANGE COLOR TO RED
 const redClick = function(x) {
@@ -24,24 +41,25 @@ const blueClick = function(x) {
     x.classList.add('blue')
 }
 
+square1.addEventListener("click", colorClick)
 //EVENT LISTENERS
-//MAKE THEM RED
-square1.addEventListener("click", () => {redClick(square1)})
-square2.addEventListener("click", () => {redClick(square2)})
-square3.addEventListener("click", () => {redClick(square3)})
-square4.addEventListener("click", () => {redClick(square4)})
-square5.addEventListener("click", () => {redClick(square5)})
-square6.addEventListener("click", () => {redClick(square6)})
-square7.addEventListener("click", () => {redClick(square7)})
-square8.addEventListener("click", () => {redClick(square8)})
-square9.addEventListener("click", () => {redClick(square9)})
-//MAKE THEM BLUE
-square1.addEventListener("click", () => {blueClick(square1)})
-square2.addEventListener("click", () => {blueClick(square2)})
-square3.addEventListener("click", () => {blueClick(square3)})
-square4.addEventListener("click", () => {blueClick(square4)})
-square5.addEventListener("click", () => {blueClick(square5)})
-square6.addEventListener("click", () => {blueClick(square6)})
-square7.addEventListener("click", () => {blueClick(square7)})
-square8.addEventListener("click", () => {blueClick(square8)})
-square9.addEventListener("click", () => {blueClick(square9)})
+// //MAKE THEM RED
+// square1.addEventListener("click", () => {redClick(square1)})
+// square2.addEventListener("click", () => {redClick(square2)})
+// square3.addEventListener("click", () => {redClick(square3)})
+// square4.addEventListener("click", () => {redClick(square4)})
+// square5.addEventListener("click", () => {redClick(square5)})
+// square6.addEventListener("click", () => {redClick(square6)})
+// square7.addEventListener("click", () => {redClick(square7)})
+// square8.addEventListener("click", () => {redClick(square8)})
+// square9.addEventListener("click", () => {redClick(square9)})
+// //MAKE THEM BLUE
+// square1.addEventListener("click", () => {blueClick(square1)})
+// square2.addEventListener("click", () => {blueClick(square2)})
+// square3.addEventListener("click", () => {blueClick(square3)})
+// square4.addEventListener("click", () => {blueClick(square4)})
+// square5.addEventListener("click", () => {blueClick(square5)})
+// square6.addEventListener("click", () => {blueClick(square6)})
+// square7.addEventListener("click", () => {blueClick(square7)})
+// square8.addEventListener("click", () => {blueClick(square8)})
+// square9.addEventListener("click", () => {blueClick(square9)})
